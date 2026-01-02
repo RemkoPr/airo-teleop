29778f2f649af6bede303b18edaa094354c1edfc
RemkoPr <remko.p@hotmail.com>
2026-01-02 13:56:49 +0100

    about to remove USE_DELTAS option

--- airo-teleop-agents/airo_teleop_agents/teleop_agent.py
+++ airo-teleop-agents/airo_teleop_agents/teleop_agent.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from airo_teleop_devices.teleop_device import TeleopDevice
 import numpy as np
 import numpy.typing as npt
+from airo_typing import HomogeneousMatrixType
 
 
 @dataclass
@@ -15,17 +16,13 @@ class TeleopAgent:
     Takes the raw state output of a TeleopDevice and transforms it to a robot action using the provided transform_func.
     The transform_func is expected to be set up properly in accordance with the TeleopConfig (e.g., joint space vs Cartesian space).
     Use of deltas is handled in get_action, based on the setting in the TeleopConfig.
-
-    TODO: consider if multiple teleop devices should be supported within a single teleop agent, for example:
-    Gello controls Mobi's manipulator, while Spacemouse controls the base. As it stands, this would require
-    separate TeleopAgents for each device.
     '''
     def __init__(self, config: TeleopAgentConfig, teleop_device: TeleopDevice, transform_func=lambda x: x):
         self.transform_func = transform_func
         self.config = config
         self.teleop_device = teleop_device
 
-    def get_action(self, current_robot_state: npt.NDArray[np.float64] | None = None) -> npt.NDArray[np.float64]:
+    def get_action(self, current_robot_state: npt.NDArray[np.float64] | None = None) -> npt.NDArray[np.float64] | tuple(HomogeneousMatrixType, float):
         teleop_device_state = self.teleop_device.get_raw_state()
         action = self.transform_func(teleop_device_state)
         if np.array(self.config.use_deltas).any():
